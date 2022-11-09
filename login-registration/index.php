4e5bf49eebb6707f49fec47f554f7d0a42a4d1a5
payless <ankur.ank1230@gmail.com>
2022-11-09 15:01:27 +0530

    first

---
+++ login-registration/index.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</head>
+<body class="h-100">
+<div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-10 col-md-8 col-lg-6">
+            <!-- Form -->
+            <form class="form-example" action="" method="post">
+                <h1>Bootstrap 4: Align Center</h1>
+                <p class="description">A tutorial on how to align a "form" vertically and horizontally in Bootstrap 4.</p>
+                <!-- Input fields -->
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" class="form-control username" id="username" placeholder="Username..." name="username">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control password" id="password" placeholder="Password..." name="password">
+                </div>
+                <button type="submit" class="btn btn-primary btn-customized">Login</button>
+                <!-- End input fields -->
+                <p class="copyright">&copy; Bootstrap 4 "Align Center" tutorial by <a href="https://azmind.com">AZMIND</a>.</p>
+            </form>
+            <!-- Form end -->
+        </div>
+    </div>
+</div>
+</body>
+</html>
