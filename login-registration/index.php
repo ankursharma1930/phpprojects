0a058af3c8aa74637f8c6eade4e65182403d5cca
payless <ankur.ank1230@gmail.com>
2022-11-10 16:08:30 +0530

    index changes

--- login-registration/index.php
+++ login-registration/index.php
@@ -24,11 +24,11 @@
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-                <form class="mx-1 mx-md-4" action="#" method="POST">
+                <form autocomplete="off" class="mx-1 mx-md-4" action="" method="post">
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" class="form-control" require/>
+                      <input type="text" id="form3Example1c" name="name" class="form-control" autocomplete="off" required/>
                       <label class="form-label" for="form3Example1c">Your Name</label>
                       <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 71.2px;"></div><div class="form-notch-trailing"></div></div>
                     </div>
@@ -37,7 +37,7 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                    <input type="email" id="email" name="email" class="form-control" required>
+                    <input type="email" id="email" name="email" autocomplete="off" class="form-control" required />
                       <label class="form-label" for="form3Example3c">Your Email</label>
                       <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 71.2px;"></div><div class="form-notch-trailing"></div></div>
                     </div>
@@ -46,7 +46,7 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" class="form-control" />
+                      <input type="password" id="form3Example4c" class="form-control" autocomplete="off" />
                       <label class="form-label" for="form3Example4c">Password</label>
                       <div class="form-notch"><div class="form-notch-leading" style="width: 9px;"></div><div class="form-notch-middle" style="width: 71.2px;"></div><div class="form-notch-trailing"></div></div>
                     </div>
@@ -62,14 +62,14 @@
                   </div>
 
                   <div class="form-check d-flex justify-content-center mb-5">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" autocomplete="off"/>
                     <label class="form-check-label" for="form2Example3">
                       I agree all statements in <a href="#!">Terms of service</a>
                     </label>
                   </div>
 
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" class="btn btn-primary btn-lg">Register</button>
+                    <button type="submit" class="btn btn-primary btn-lg">Register</button>
                   </div>
 
                 </form>
